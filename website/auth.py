0ffc9bca92f52d3aa17814865446ff400d636c35
Novi Patricia <novi.patricia@gmail.com>
2021-04-18 22:41:43 +0700

    commit

--- website/auth.py
+++ website/auth.py
@@ -2,8 +2,9 @@ from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+    data = request.form()
     return render_template("login.html", text="Testing")
 
 @auth.route("/logout")
