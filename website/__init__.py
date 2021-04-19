28b36640da26515792627e1300b453526f07609c
Novi Patricia <novi.patricia@gmail.com>
2021-04-19 11:52:53 +0700

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+import os
 
 db = SQLAlchemy()
+
+def create_database(app):
+    if not os.path.exists(f'website/mynote.db'):
+        db.create_all(app=app)
+        print('Created database')
 
 def create_app():
     app = Flask(__name__)
@@ -15,5 +23,17 @@ def create_app():
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
